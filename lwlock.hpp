ad40770d73148dd9af6b1b9b3ea642600bd2f5e3
Agustina Arzille <avarzille@riseup.net>
2018-09-13 16:51:07 -0300

    Document lightweight locks

--- lwlock.hpp
+++ lwlock.hpp
@@ -6,12 +6,20 @@
 namespace xrcu
 {
 
+/* Lightweight lock used when including <mutex> is overkill. Should be just as
+ * performant in certain platforms (i.e: Those that support something akin to
+ * Linux's futexes), but much lighter space-wise. */
 struct lwlock
 {
   uintptr_t lock = 0;
 
   void acquire ();
   void release ();
+
+  lwlock () = default;
+
+  lwlock (const lwlock&) = delete;
+  void operator= (const lwlock&) = delete;
 };
 
 }
